7ec7a865e6abe8f197c1f7b1aebba70234764a8c
kelee <kelee5453@outllok.com>
2021-04-18 16:11:12 +0800

    add k210-example-nodvp

---
+++ k210-example/build/CMakeFiles/face-detect.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face-detect.dir/src/lcd.c.obj"
+  "CMakeFiles/face-detect.dir/src/main.c.obj"
+  "CMakeFiles/face-detect.dir/src/nt35310.c.obj"
+  "CMakeFiles/face-detect.dir/src/ov2640.c.obj"
+  "CMakeFiles/face-detect.dir/src/ov5640.c.obj"
+  "CMakeFiles/face-detect.dir/src/region_layer.c.obj"
+  "CMakeFiles/face-detect.dir/src/w25qxx.c.obj"
+  "face-detect.pdb"
+  "face-detect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/face-detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/DependInfo.cmake
@@ -0,0 +1,112 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/crt.S" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/crt.S.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/entry.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/entry.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/entry_user.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/entry_user.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/interrupt.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/interrupt.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/locks.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/locks.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/printf.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/printf.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/sleep.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/sleep.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/syscalls.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/bsp/syscalls.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/aes.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/aes.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/apu.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/apu.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/clint.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/clint.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/dmac.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/dmac.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/dvp.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/dvp.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/fft.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/fft.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/fpioa.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/fpioa.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/gpio.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/gpio.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/gpiohs.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/gpiohs.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/i2c.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/i2c.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/i2s.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/i2s.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/iomem.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/iomem.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/kpu.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/kpu.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/plic.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/plic.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/pwm.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/pwm.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/rtc.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/rtc.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/sha256.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/sha256.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/spi.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/spi.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/sysctl.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/sysctl.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/timer.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/timer.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/uart.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/uart.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/uarths.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/uarths.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/utils.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/utils.c.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/drivers/wdt.c" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/drivers/wdt.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CONFIG_LOG_ENABLE"
+  "CONFIG_LOG_LEVEL=LOG_INFO"
+  "DEBUG=1"
+  "KENDRYTE_SDK_RELEASE_DATE=0"
+  "KENDRYTE_SDK_TYPE=1"
+  "KENDRYTE_SDK_TYPE_FREERTOS=2"
+  "KENDRYTE_SDK_TYPE_STANDALONE=1"
+  "NNCASE_TARGET=k210"
+  "PROJECT_PATH=/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/"
+  "TCB_SPAN_NO_CONTRACT_CHECKING"
+  "TCB_SPAN_NO_EXCEPTIONS"
+  "__riscv64"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../config"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/bsp/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/drivers/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/utils/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/third_party/xtl/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/nncase/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/config"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/nncase/nncase.cpp" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/nncase/nncase.cpp.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/nncase/runtime/cpu/cpu_ops.cpp" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/nncase/runtime/cpu/cpu_ops.cpp.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/nncase/runtime/interpreter.cpp" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/nncase/runtime/interpreter.cpp.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/nncase/runtime/k210/interpreter.cpp" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/nncase/runtime/k210/interpreter.cpp.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/nncase/runtime/k210/k210_ops.cpp" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/nncase/runtime/k210/k210_ops.cpp.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/nncase/runtime/kernel_registry.cpp" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/nncase/runtime/kernel_registry.cpp.obj"
+  "/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/lib/nncase/runtime/neutral/neutral_ops.cpp" "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/lib/nncase/runtime/neutral/neutral_ops.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CONFIG_LOG_ENABLE"
+  "CONFIG_LOG_LEVEL=LOG_INFO"
+  "DEBUG=1"
+  "KENDRYTE_SDK_RELEASE_DATE=0"
+  "KENDRYTE_SDK_TYPE=1"
+  "KENDRYTE_SDK_TYPE_FREERTOS=2"
+  "KENDRYTE_SDK_TYPE_STANDALONE=1"
+  "NNCASE_TARGET=k210"
+  "PROJECT_PATH=/home/kelee/Desktop/k210-example/kendryte_libraries/kendryte_standalone-sdk/"
+  "TCB_SPAN_NO_CONTRACT_CHECKING"
+  "TCB_SPAN_NO_EXCEPTIONS"
+  "__riscv64"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../config"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/bsp/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/drivers/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/utils/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/third_party/xtl/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/nncase/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/config"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
