baebfa278acd91bc567ce8e4b32841af49f13cc0
kelee <kelee5453@outllok.com>
2021-05-11 20:44:24 +0800

    存在传值问题

--- lab8/user/hello.c
+++ lab8/user/hello.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <ulib.h>
-#define printf(...)                     fprintf(1, __VA_ARGS__)
-#define kpuprintf(...)                     fprintf(2, __VA_ARGS__)
-#define putc(c)                         printf("%c", c)
-char ttt[5]; 
-struct kpu_buff{
+#define printf(...) fprintf(1, __VA_ARGS__)
+#define kpuprintf(...) fprintf(2, __VA_ARGS__)
+#define putc(c) printf("%c", c)
+char ttt[5] = "abcd";
+struct kpu_buff
+{
     uint64_t size;
-    char* jpeg;
-}buff;
-int
-main(void) {
+    char *jpeg;
+} buff;
+int main(void)
+{
     buff.jpeg = &ttt[0];
     buff.size = 5;
     printf("Hello world!!.\n");
-    cprintf("buff.jpeg %p\n", buff.jpeg);
-    cprintf("buff.size %d\n", buff.size);
+    cprintf("buff.jpeg %x\n", buff.jpeg);
+    cprintf("buff.size %d\n", sizeof(buff));
     cprintf("buff. %p\n", &buff);
 
     cprintf("hello pass.\n");
-    for(int i = 0; i < 100; i++) ttt[i] = i+10;
-    int status = kpuprintf("%x", &buff, 1);
+    for (int i = 0; i < 100; i++)
+        ttt[i] = i + 10;
+    int status = kpuprintf("%x", &buff, sizeof(buff));
     cprintf("I am process %d.\n", getpid());
     return 0;
 }
-
