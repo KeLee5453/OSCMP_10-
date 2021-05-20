b479ed309d742805593b8a8257ff6b69b29558e2
kelee <kelee5453@outllok.com>
2021-05-20 11:46:10 +0800

    新的例子

---
+++ k210-example/build/CMakeFiles/face-detect.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kelee/Desktop/k210-example/src/cnn.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/cnn.c.obj"
+  "/home/kelee/Desktop/k210-example/src/dog.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/dog.c.obj"
+  "/home/kelee/Desktop/k210-example/src/dog888.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/dog888.c.obj"
+  "/home/kelee/Desktop/k210-example/src/gencode_output.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/gencode_output.c.obj"
+  "/home/kelee/Desktop/k210-example/src/lcd.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/lcd.c.obj"
+  "/home/kelee/Desktop/k210-example/src/main.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/main.c.obj"
+  "/home/kelee/Desktop/k210-example/src/nt35310.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/nt35310.c.obj"
+  "/home/kelee/Desktop/k210-example/src/ov2640.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/ov2640.c.obj"
+  "/home/kelee/Desktop/k210-example/src/ov5640.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/ov5640.c.obj"
+  "/home/kelee/Desktop/k210-example/src/picojpeg.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/picojpeg.c.obj"
+  "/home/kelee/Desktop/k210-example/src/picojpeg_util.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/picojpeg_util.c.obj"
+  "/home/kelee/Desktop/k210-example/src/region_layer.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/region_layer.c.obj"
+  "/home/kelee/Desktop/k210-example/src/w25qxx.c" "/home/kelee/Desktop/k210-example/build/CMakeFiles/face-detect.dir/src/w25qxx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CONFIG_LOG_ENABLE"
+  "CONFIG_LOG_LEVEL=LOG_INFO"
+  "DEBUG=1"
+  "KENDRYTE_SDK_RELEASE_DATE=0"
+  "KENDRYTE_SDK_TYPE=1"
+  "KENDRYTE_SDK_TYPE_FREERTOS=2"
+  "KENDRYTE_SDK_TYPE_STANDALONE=1"
+  "NNCASE_TARGET=k210"
+  "PROJECT_PATH=/home/kelee/Desktop/k210-example/"
+  "TCB_SPAN_NO_CONTRACT_CHECKING"
+  "TCB_SPAN_NO_EXCEPTIONS"
+  "__riscv64"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../config"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/bsp/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/drivers/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/utils/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/third_party/xtl/include"
+  "../kendryte_libraries/kendryte_standalone-sdk/lib/nncase/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kelee/Desktop/k210-example/build/kendryte_standalone-sdk/CMakeFiles/kendryte_standalone-sdk.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ k210-example/build/CMakeFiles/face-detect.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face-detect.dir/src/cnn.c.obj"
+  "CMakeFiles/face-detect.dir/src/dog.c.obj"
+  "CMakeFiles/face-detect.dir/src/dog888.c.obj"
+  "CMakeFiles/face-detect.dir/src/gencode_output.c.obj"
+  "CMakeFiles/face-detect.dir/src/lcd.c.obj"
+  "CMakeFiles/face-detect.dir/src/main.c.obj"
+  "CMakeFiles/face-detect.dir/src/nt35310.c.obj"
+  "CMakeFiles/face-detect.dir/src/ov2640.c.obj"
+  "CMakeFiles/face-detect.dir/src/ov5640.c.obj"
+  "CMakeFiles/face-detect.dir/src/picojpeg.c.obj"
+  "CMakeFiles/face-detect.dir/src/picojpeg_util.c.obj"
+  "CMakeFiles/face-detect.dir/src/region_layer.c.obj"
+  "CMakeFiles/face-detect.dir/src/w25qxx.c.obj"
+  "face-detect.pdb"
+  "face-detect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/face-detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
